b4fdf3bef5940946d29f3433b3c812d762c407fd
Kim-Dong-ill <150985297+Kim-Dong-ill@users.noreply.github.com>
2024-03-26 15:52:44 +0900

    react

---
+++ 2024react/my-app/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+const a = "test입니다.";
+function App() {
+  return (
+    <div className="App">
+      <h3 className="title">{a}</h3>
+    </div>
+  );
+}
+
+export default App;
