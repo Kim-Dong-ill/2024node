050df22992c5daef3204ccd0bac0ec942e8549fb
Kim-Dong-ill <150985297+Kim-Dong-ill@users.noreply.github.com>
2024-03-27 09:51:45 +0900

    adwq

--- 01-node/2024-FILE_UPLOAD/src/routes/userRouter.js
+++ 01-node/2024-FILE_UPLOAD/src/routes/userRouter.js
@@ -45,4 +45,13 @@ userRouter.get("/:userId", async function (req, res) {
   }
 });
 
+userRouter.get("/", async function (req, res) {
+  try {
+    const user = await User.find();
+    return res.send({ user });
+  } catch (error) {
+    res.status(500).send({ error: error.message });
+  }
+});
+
 module.exports = { userRouter };
