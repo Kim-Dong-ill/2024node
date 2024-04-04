76327873447e9d078cb3d4545bc3e12c9bcd0faf
Kim-Dong-ill <150985297+Kim-Dong-ill@users.noreply.github.com>
2024-04-04 09:56:25 +0900

    react

--- 2024react/my-app/src/App.js
+++ 2024react/my-app/src/App.js
@@ -1,51 +1,67 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import "./App.css";
+import ModalView from "./components/ModalView";
 
 function App() {
-  const [list, setList] = useState(["1. 첫번째", "2. 두번째", "3. 세번째"]);
-  const [tabNum, setTabNum] = useState(1);
-  const [modalView, setModalView] = useState(false);
-  const switchModalClick = () => {
-    setModalView(!modalView);
-  };
+  let [mView, setMView] = useState(false);
+  let viewList = [
+    {
+      title: " 안녕하세요!!",
+      content:
+        " Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere, alias",
+    },
+    {
+      title: " 만2나서 반갑습니다.",
+      content:
+        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere, alias",
+    },
+    {
+      title: " 만3나서 반갑습니다.",
+      content:
+        " Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere, alias",
+    },
+    {
+      title: " 만4나서 반갑습니다.",
+      content:
+        " Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere, alias",
+    },
+    {
+      title: " 만5나서 반갑습니다.",
+      content:
+        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere, alias",
+    },
+  ];
+
+  let [vNum, setVNum] = useState(0);
+
+  function modalView(idx) {
+    setMView(true);
+    setVNum(idx);
+  }
+
+  function modalClose() {
+    setMView(false);
+  }
 
   return (
     <>
-      <div className="tabWrap">
-        <ul className="tabMenu">
-          <li
-            className={`${tabNum == 0 ? "active" : null}`}
-            onClick={() => {
-              setTabNum(0);
-            }}
-          >
-            tab1
-          </li>
-          <li
-            className={`${tabNum == 1 ? "active" : null}`}
-            onClick={() => {
-              setTabNum(1);
-            }}
-          >
-            tab2
-          </li>
-          <li
-            className={`${tabNum == 2 ? "active" : null}`}
-            onClick={() => {
-              setTabNum(2);
-            }}
-          >
-            tab3
-          </li>
+      <div>
+        <ul>
+          {viewList.map((item, idx) => {
+            return (
+              <li
+                onClick={() => {
+                  modalView(idx);
+                }}
+              >
+                {idx + 1}. {item.title}
+              </li>
+            );
+          })}
         </ul>
       </div>
-      <div>{list[tabNum]}</div>
-      <button onClick={switchModalClick}>모달창</button>
-      {modalView == true ? (
-        <div className="modals">
-          <h4>모달창</h4>
-          <p>모달창 내용...</p>
-        </div>
+      {mView == true ? (
+        <ModalView onClick={modalClose} viewListdata={viewList} vNum={vNum} />
       ) : null}
     </>
   );
