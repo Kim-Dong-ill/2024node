3ae2239cd783cb2841b0972bd5b5b543272ec3d8
Kim-Dong-ill <150985297+Kim-Dong-ill@users.noreply.github.com>
2024-04-09 10:27:14 +0900

    aa

--- 2024react/router/src/App.js
+++ 2024react/router/src/App.js
@@ -1,41 +1,20 @@
-import "./App.css";
-import { Routes, Route, NavLink } from "react-router-dom";
+import React from "react";
+import { vData } from "./data";
+
+import Header from "./layout/Header";
+import Footer from "./layout/Footer";
+import Main from "./layout/Main";
+
 import "./assets/css/style.scss";
 
 function App() {
   return (
     <>
-      <div className="container">
-        <h1>Logo</h1>
-        <ul className="menu">
-          <li>
-            <NavLink to="/">home</NavLink>
-          </li>
-          <li>
-            <NavLink to="/company">회사소개</NavLink>
-          </li>
-          <li>
-            <NavLink to="/product">제품소개</NavLink>
-          </li>
-          <li>
-            <NavLink to="/community">게시판</NavLink>
-          </li>
-        </ul>
-      </div>
-      <div className="section">
-        <Routes>
-          <Route path="/" element={<div>메인</div>} />
-          <Route path="/company" element={<div>회사소개</div>} />
-          <Route path="/product" element={<div>제품소개</div>} />
-          <Route path="/community" element={<div>게시판</div>} />
-        </Routes>
-      </div>
-      <div className="footer">
-        <div>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
-          odit!
-        </div>
-      </div>
+      <Header></Header>
+
+      <Main vData={vData}></Main>
+
+      <Footer></Footer>
     </>
   );
 }
